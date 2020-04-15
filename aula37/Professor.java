d0835479fcce14fd72c1fe36bf06b6b2ffe52837
daneccf <danielecavalcante1@hotmail.com>
2020-04-15 09:39:05 -0300

    aula37

---
+++ aula37/Professor.java
@@ -0,0 +1,12 @@
+package aula37;
+
+public class Professor extends Pessoa {
+
+    private double salario;
+    private String nomeCurso;
+
+    public double calcularSalarioLiquido() {
+        return 0;
+    }
+
+}
