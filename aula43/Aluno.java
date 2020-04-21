8aa6211a528f0180c6df705fc977531e4a044cf7
daneccf <danielecavalcante1@hotmail.com>
2020-04-21 15:58:35 -0300

    aula43

--- aula43/Aluno.java
+++ aula43/Aluno.java
@@ -1,9 +1,10 @@
 package aula43;
 
-import java.util.Arrays;
-import java.util.Objects;
 
-public class Aluno {
+
+import java.util.Arrays;
+
+public class Aluno  {
 
     private String curso;
     private double[] notas;
@@ -13,84 +14,83 @@ public class Aluno {
     }
 
     public Aluno(String nome, String endereco, String telefone, String curso) {
-//        super(nome, endereco, telefone);
+        //super(nome, endereco, telefone);
         this.curso = curso;
     }
 
     public String getCurso() {
         return curso;
     }
-
     public void setCurso(String curso) {
         this.curso = curso;
     }
-
-
     public double[] getNotas() {
         return notas;
     }
-
     public void setNotas(double[] notas) {
         this.notas = notas;
     }
 
-
-    public double calcularMedia() {
+    public double calcularMedia(){
         return 0;
     }
 
-    public boolean verificarAprovado() {
+    public boolean verificarAprovado(){
         return true;
     }
 
-    public String obterEtiquetaEndereco() {
-        String s = "Endereço do Professor";
-//        s += super.getEndereco();
+    public void metodoQualquer(){
+        //super.setCpf("345345345354");
+
+        //this.setCpf("34534534");
+    }
+
+    public String obterEtiquetaEndereco(){
+
+        String s = "Endereço do Aluno: ";
+        //s += super.getEndereco();
 
         return s;
     }
 
-
-    @SuppressWarnings ({"UnusedDeclaration"})
-//    @Override
+    //@Override
     public void imprimirEtiquetaEndereco() {
+
         System.out.println(this.obterEtiquetaEndereco());
+
     }
 
-/*
+    @Override
     public String toString() {
-        String s = curso + "\n";
-        for (double nota : notas) {
-            s += nota + " ";
+        return "Aluno [curso=" + curso + ", notas=" + Arrays.toString(notas)
+                + "]";
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+
+        Aluno other = (Aluno) obj;
+
+        if (curso.equalsIgnoreCase(other.getCurso())){
+            return true;
         }
-        return s;
+
+        return false;
     }
 
-*/
+	/*public String toString(){
+		String s = curso + "\n";
+		for (double nota : notas){
+			s += nota + " ";
+		}
+		return s;
+	}*/
 
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Aluno)) return false;
-        Aluno aluno = (Aluno) o;
-        return getCurso().equals(aluno.getCurso()) &&
-                Arrays.equals(getNotas(), aluno.getNotas());
-    }
-//
-//    @Override
-//    public int hashCode() {
-//        int result = Objects.hash(getCurso());
-//        result = 31 * result + Arrays.hashCode(getNotas());
-//        return result;
-//    }
-
-    @Override
-    public String toString() {
-        return "Aluno{" +
-                "curso='" + curso + '\'' +
-                ", notas=" + Arrays.toString(notas) +
-                '}';
-    }
 }
  
