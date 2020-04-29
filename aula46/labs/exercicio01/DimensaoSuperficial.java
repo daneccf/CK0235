560501bf038d2b77a07dbb176b5f93466068272e
daneccf <danielecavalcante1@hotmail.com>
2020-04-28 23:00:46 -0300

    adicionando interfaces

---
+++ aula46/labs/exercicio01/DimensaoSuperficial.java
@@ -0,0 +1,7 @@
+package aula46.labs.exercicio01;
+
+public interface DimensaoSuperficial {
+    //todo metodo eh public abstract
+    double calcularArea();
+
+}
---
+++ aula46/labs/exercicio01/DimensaoVolumetrica.java
@@ -0,0 +1,7 @@
+package aula46.labs.exercicio01;
+
+public interface DimensaoVolumetrica {
+
+    double calcularArea();
+    
+}
