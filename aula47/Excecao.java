5718c414ab05d1923013f47f04c02b413f3d0ff1
daneccf <danielecavalcante1@hotmail.com>
2020-04-30 23:13:00 -0300

    exception

---
+++ aula47/Excecao.java
@@ -0,0 +1,20 @@
+package aula47;
+
+public class Excecao {
+
+    public static void main(String[] args) {
+
+        try {
+            int[] vetor = new int[4];
+            System.out.println("Antes da exception.");
+
+            vetor[4] = 1;
+            System.out.println("Esse texto nao sera impresso");
+        } catch(ArrayIndexOutOfBoundsException exception) {
+            System.out.println("Excecão ao acessar um índice que não existe.");
+        }
+
+        System.out.println("Esse texto será impresso após a exception.");
+
+    }
+}
