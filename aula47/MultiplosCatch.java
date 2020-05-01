2e4a68f8e44dddc12876f7b86e5146f8e8d14ced
daneccf <danielecavalcante1@hotmail.com>
2020-04-30 23:29:09 -0300

    multiplos catch

---
+++ aula47/MultiplosCatch.java
@@ -0,0 +1,20 @@
+package aula47;
+
+public class MultiplosCatch {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {4, 8, 16, 32, 64, 128};
+        int[] denomin = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numeros.length; i++) {
+            try {
+                System.out.println(numeros[i] + "/" + denomin[i] + "=" + (numeros[i] / denomin[i]));
+            } catch (ArithmeticException e) {
+                System.out.println("Erro ao dividir por zero.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Posição do array inválida.");
+            }
+        }
+    }
+}
