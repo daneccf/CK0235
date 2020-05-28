a87fb7a8be22c2e5dd42e053cb901b5cc5278099
daneccf <danielecavalcante1@hotmail.com>
2020-05-28 15:15:28 -0300

    added menu

--- aula52/Agenda.java
+++ aula52/Agenda.java
@@ -25,6 +25,18 @@ public class Agenda {
         }
     }
 
+    public int consultaContatoPorNome(String nome) throws ContatoNaoExisteException {
+
+        for (int i = 0; i < contatos.length; i++) {
+            if (contatos[i] != null) {
+                if (contatos[i].getNome().equalsIgnoreCase(nome)) {
+                    return i;
+                }
+            }
+        }
+        throw new ContatoNaoExisteException(nome);
+    }
+
     @Override
     public String toString() {
         return "Agenda {" +
--- aula52/Teste.java
+++ aula52/Teste.java
@@ -1,18 +1,42 @@
 package aula52;
 
+import java.util.Scanner;
+
 public class Teste {
 
     public static void main(String[] args) {
 
-        Contato c1 = new Contato();
-        System.out.println(c1);
+        Scanner scan = new Scanner(System.in);
 
-        Contato c2 = new Contato();
-        System.out.println(c2);
+        int opcao = obterOpcaoMenu(scan);
+    }
 
-        Contato c3 = new Contato();
-        System.out.println(c3);
+    public static int obterOpcaoMenu(Scanner scan) {
 
+        boolean entradaValida = false;
+        int opcao = 3;
 
+        while(!entradaValida) {
+            System.out.println("Digite a opção desejada: ");
+            System.out.println("1: consultar contato");
+            System.out.println("2: adicionar contato");
+            System.out.println("3: sair");
+
+            try{
+                String entrada = scan.nextLine();
+                opcao = Integer.parseInt(entrada);
+
+                if (opcao == 1 || opcao == 2 || opcao == 3) {
+                    entradaValida = true;
+                } else {
+                    throw new Exception("Entrada inválida ");
+                }
+            } catch (Exception e) {
+                System.out.println("Entrada inválida, digite novamente\n ");
+
+            }
+        }
+
+        return opcao;
     }
 }
