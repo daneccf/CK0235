81b2bbe3949dad519e30d8974d4a20bb85b45491
daneccf <danielecavalcante1@hotmail.com>
2020-06-01 14:45:41 -0300

    enum

--- aula54/TesteEnum.java
+++ aula54/TesteEnum.java
@@ -7,5 +7,7 @@ public class TesteEnum {
         DiaSemana dia = DiaSemana.DOMINGO;
 
         System.out.println(dia.toString() + " - " + dia.getValor());
+
+        Data data = new Data(1, 4, 2016, DiaSemana.SEXTA);
     }
 }
